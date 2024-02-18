94c78517da1ab9f20021a9343563f31dd4c54287
Arron <GreenArronic@gmail.com>
2024-02-19 01:47:23 +0330

    Write JSX logic for writting & syncing form state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 import DocPreview from "./components/DocPreview";
+import InputField from "./components/InputField";
+
+let nextId = 0;
 
 function App() {
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [bio, setBio] = useState("");
+  const [educations, setEducations] = useState([]);
+  // {label, date, id}
+  const [crimes, setCrimes] = useState([]);
+  const [phonenumber, setPhonenumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
   return (
-    <main>
-      <div></div>
-      <DocPreview />
-    </main>
+    <>
+      <InputField
+        onNameChange={setName}
+        onDobChange={setDob}
+        onBioChange={setBio}
+        educations={educations}
+        onEducationsChange={setEducations}
+        onPhonenumberChange={setPhonenumber}
+        crimes={crimes}
+        onCrimesChange={setCrimes}
+        onEmailChange={setEmail}
+        onAddressChange={setAddress}
+      />
+      <div className="preview-container">
+        <DocPreview
+          name={name}
+          dob={dob}
+          bio={bio}
+          educations={educations}
+          crimes={crimes}
+          phonenumber={phonenumber}
+          email={email}
+          address={address}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/InputField.jsx
@@ -0,0 +1,165 @@
+import { useState } from "react";
+import "./InputField.css";
+
+function Input({ placeholder, label, id, onChange, type = "text" }) {
+  function handleChange(e) {
+    if (!!onChange) {
+      onChange(e.target.value);
+    }
+  }
+
+  switch (type) {
+    case "textarea":
+      return (
+        <>
+          <label htmlFor={id}>{label}</label>
+          <textarea
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            onChange={handleChange}
+            rows="4"
+          />
+        </>
+      );
+
+    default:
+      return (
+        <>
+          <label htmlFor={id}>{label}</label>
+          <input
+            type={type}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            onChange={handleChange}
+          />
+        </>
+      );
+  }
+}
+
+function ListInput({ label, id, placeholder, items, onAddItem }) {
+  const [currentInput, setCurrentInput] = useState("");
+  function handleChange(e) {
+    setCurrentInput(e.target.value);
+  }
+  function handleAdd() {
+    if (currentInput.trim() !== "") {
+      items = [...items, currentInput];
+      setCurrentInput("");
+      onAddItem(items);
+    }
+  }
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type="text"
+        id={id}
+        placeholder={placeholder}
+        onChange={handleChange}
+        value={currentInput}
+      />
+      <div id={`${id}collection`}>
+        {items.map((item) => {
+          return <div>{item}</div>;
+        })}
+      </div>
+      <button className="addtolist" onClick={handleAdd}>
+        ➕
+      </button>
+    </>
+  );
+}
+
+function Card({ children }) {
+  return <div className="inputcard">{children}</div>;
+}
+
+export default function InputField({
+  onNameChange,
+  onDobChange,
+  onBioChange,
+  educations,
+  onEducationsChange,
+  crimes,
+  onCrimesChange,
+  onPhonenumberChange,
+  onEmailChange,
+  onAddressChange,
+}) {
+  /*
+    name,
+  dob,
+  bio,
+  educations,
+  crimes,
+  phonenumber,
+  email,
+  address,
+    */
+  return (
+    <div className="inputfield">
+      <Card>
+        <Input
+          placeholder="Jack..."
+          label="NAME"
+          id="namefield"
+          onChange={onNameChange}
+        />
+        <Input
+          placeholder="May 21, 1999..."
+          label="BIRTHDAY"
+          id="dobfield"
+          onChange={onDobChange}
+        />
+        <Input
+          placeholder="One upon a time I went to the store..."
+          label="BIO"
+          id="biofield"
+          type="textarea"
+          onChange={onBioChange}
+        />
+      </Card>
+      <Card>
+        <Input
+          placeholder="+1-123-456789"
+          label="PHONENUMBER"
+          id="phonenumber"
+          onChange={onPhonenumberChange}
+        />
+        <Input
+          placeholder="JackCrawford@fake.com"
+          label="EMAIL"
+          id="email"
+          onChange={onEmailChange}
+        />
+        <Input
+          placeholder="308 Negra Arroyo Lane, Albuquerque, New Mexico..."
+          label="ADDRESS"
+          id="address"
+          onChange={onAddressChange}
+        />
+      </Card>
+      <Card>
+        <ListInput
+          placeholder="College name, Course..."
+          id="educations"
+          label="EDUCATION"
+          items={educations}
+          onAddItem={onEducationsChange}
+        />
+      </Card>
+      <Card>
+        <ListInput
+          placeholder="Felony, First class offense..."
+          id="crimes"
+          label="CRIME"
+          items={crimes}
+          onAddItem={onCrimesChange}
+        />
+      </Card>
+    </div>
+  );
+}
